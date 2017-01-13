f1d134c64a32059b19c51d831ed7b69d5e1fb2c4
Evanito <yopu1234@gmail.com>
2017-01-13 14:47:31 -0800

    Optimize changes

--- modules/Lending.py
+++ modules/Lending.py
@@ -30,7 +30,7 @@ hide_coins = True
 
 # limit of orders to request
 loanOrdersRequestLimit = {}
-defaultLoanOrdersRequestLimit = 200
+defaultLoanOrdersRequestLimit = 100
 
 
 def init(cfg, api1, log1, data, maxtolend, dry_run1, analysis):
@@ -68,6 +68,14 @@ def init(cfg, api1, log1, data, maxtolend, dry_run1, analysis):
 
 def get_sleep_time():
     return sleep_time
+
+
+def set_sleep_time(usable):
+    global sleep_time
+    if usable == 0:  # After loop, if no currencies had enough to lend, use inactive sleep time.
+        sleep_time = sleep_time_inactive
+    else:  # Else, use active sleep time.
+        sleep_time = sleep_time_active
 
 
 def get_min_loan_size(currency):
@@ -150,10 +158,7 @@ def lend_all():
             usable_currencies += lend_cur(cur, total_lended, lending_balances)
         except StopIteration:  # Restart lending if we stop to raise the request limit.
             lend_all()
-    if usable_currencies == 0:  # After loop, if no currencies had enough to lend, use inactive sleep time.
-        sleep_time = sleep_time_inactive
-    else:  # Else, use active sleep time.
-        sleep_time = sleep_time_active
+    set_sleep_time(usable_currencies)
 
 
 def get_min_daily_rate(cur):
@@ -185,7 +190,7 @@ def construct_order_book(active_cur):
     for offer in loans['offers']:
         rate_book.append(offer['rate'])
         volume_book.append(offer['amount'])
-    return [rate_book, volume_book]
+    return {'rates': rate_book, 'volumes': volume_book}
 
 
 def get_gap_rate(active_cur, gap_pct, order_book, cur_active_bal):
@@ -194,47 +199,42 @@ def get_gap_rate(active_cur, gap_pct, order_book, cur_active_bal):
     i = -1
     while gap_sum < gap_expected:
         i += 1
-        if i == len(order_book[1]) and len(order_book[1]) == loanOrdersRequestLimit[active_cur]:
+        if i == len(order_book['volumes']) and len(order_book['volumes']) == loanOrdersRequestLimit[active_cur]:
             loanOrdersRequestLimit[active_cur] += defaultLoanOrdersRequestLimit
             log.log(active_cur + ': Not enough offers in response, adjusting request limit to ' + str(
                 loanOrdersRequestLimit[active_cur]))
             raise StopIteration
-        gap_sum += float(order_book[1][i])
-    return Decimal(order_book[0][i])
+        gap_sum += float(order_book['volumes'][i])
+    return Decimal(order_book['rates'][i])
 
 
-def get_order_amounts(spread, cur_active_bal, active_cur):
+def get_cur_spread(spread, cur_active_bal, active_cur):
     cur_spread_lend = int(spread)  # Checks if active_bal can't be spread that many times, and may go down to 1.
-    while cur_active_bal < (cur_spread_lend * get_min_loan_size(active_cur)):
+    cur_min_loan_size = get_min_loan_size(active_cur)
+    while cur_active_bal < (cur_spread_lend * cur_min_loan_size):
         cur_spread_lend -= 1
-    i = 0
-    order_amounts = []
-    while i < cur_spread_lend:
-        order_amounts.append(cur_active_bal / cur_spread_lend)
-        i += 1
-    return order_amounts
+    return cur_spread_lend
 
 
 def construct_orders(cur, cur_active_bal):
-    order_amounts = get_order_amounts(spread_lend, cur_active_bal, cur)
+    cur_spread = get_cur_spread(spread_lend, cur_active_bal, cur)
     order_book = construct_order_book(cur)
     bottom_rate = get_gap_rate(cur, gap_bottom, order_book, cur_active_bal)
     top_rate = get_gap_rate(cur, gap_top, order_book, cur_active_bal)
 
     gap_diff = top_rate - bottom_rate
-    if len(order_amounts) == 1:
+    if cur_spread == 1:
         rate_step = 0
     else:
-        rate_step = gap_diff / (len(order_amounts) - 1)
+        rate_step = gap_diff / (cur_spread - 1)
 
     order_rates = []
     i = 0
-    while i < len(order_amounts):
+    while i < cur_spread:
         new_rate = bottom_rate + (rate_step * i)
         order_rates.append(new_rate)
         i += 1
     # Condensing and logic'ing time
-    amounts = sum(order_amounts)
     for rate in order_rates:
         if rate > max_daily_rate:
             order_rates[rate] = max_daily_rate
@@ -242,10 +242,13 @@ def construct_orders(cur, cur_active_bal):
     new_order_amounts = []
     i = 0
     while i < len(new_order_rates):
-        new_amount = Data.truncate(amounts / len(new_order_rates), 8)
+        new_amount = Data.truncate(cur_active_bal / len(new_order_rates), 8)
         new_order_amounts.append(new_amount)
         i += 1
-    return [new_order_amounts, new_order_rates]
+    remainder = cur_active_bal - sum(new_order_amounts)
+    if remainder > 0:  # If truncating causes remainder, add that to first order.
+        new_order_amounts[0] += remainder
+    return {'amounts': new_order_amounts, 'rates': new_order_rates}
 
 
 def lend_cur(active_cur, total_lended, lending_balances):
@@ -259,11 +262,11 @@ def lend_cur(active_cur, total_lended, lending_balances):
     # log total coin
     log.updateStatusValue(active_cur, "totalCoins", (Decimal(active_cur_total_balance)))
     order_book = construct_order_book(active_cur)
-    if not order_book or len(order_book[0]) == 0:
+    if not order_book or len(order_book['rates']) == 0:
         return 0
 
     active_bal = MaxToLend.amount_to_lend(active_cur_total_balance, active_cur, Decimal(lending_balances[active_cur]),
-                                          Decimal(order_book[0][0]))
+                                          Decimal(order_book['rates'][0]))
 
     if float(active_bal) > get_min_loan_size(active_cur):  # Make sure sleeptimer is set to active if any cur can lend.
         currency_usable = 1
@@ -272,17 +275,19 @@ def lend_cur(active_cur, total_lended, lending_balances):
 
     orders = construct_orders(active_cur, active_bal)  # Construct all the potential orders
     i = 0
-    while i < len(orders[0]):  # Iterate through prepped orders and create them if they work
-        below_min = orders[1][i] < Decimal(cur_min_daily_rate)
+    while i < len(orders['amounts']):  # Iterate through prepped orders and create them if they work
+        below_min = orders['amounts'][i] < Decimal(cur_min_daily_rate)
+
         if hide_coins and below_min:
             log.log("Not lending " + active_cur + " due to low rate.")
             return 0
         elif below_min:
             rate = min_daily_rate
         else:
-            rate = orders[1][i]
+            rate = orders['rates'][i]
+
         try:
-            create_lend_offer(active_cur, orders[0][i], rate)
+            create_lend_offer(active_cur, orders['amounts'][i], rate)
         except Exception as msg:
             if "Amount must be at least " in str(msg):
                 import re
